d4456611e2a6368779acbd361c06d7e4793ddd3b
mahery76 <randrianjafymahery@gmail.com>
2023-06-16 22:56:26 +0300

    Migrating from memoire to devs

---
+++ contrainte.js
@@ -0,0 +1,82 @@
+let classes = [
+    {
+        "info L1": {
+            "matiere1": 20,
+            "matiere2": 30,
+            "matiere3": 30,
+            "matiere4": 30,
+            "matiere5": 30,
+            "matiere6": 20,
+            "matiere7": 20,
+        }
+    },
+    {
+        "info L2": {
+            "matiere8": 20,
+            "matiere9": 30,
+            "matiere10": 30,
+            "matiere11": 30,
+            "matiere12": 30,
+            "matiere13": 30,
+            "matiere14": 30,
+        }
+    }
+]
+
+let creneaux = {
+    "Lun 1": "Lundi 8:30 - 10:30",
+    "Lun 2": "Lundi 10:30 - 12:30",
+    "Lun 3": "Lundi 13:30 - 15:30",
+    "Lun 4": "Lundi 15:30 - 17:30",
+
+    "Mar 1": "Mardi 8:30 - 10:30",
+    "Mar 2": "Mardi 10:30 - 12:30",
+    "Mar 3": "Mardi 13:30 - 15:30",
+    "Mar 4": "Mardi 15:30 - 17:30",
+
+    "Mer 1": "Mercredi 8:30 - 10:30",
+    "Mer 2": "Mercredi 10:30 - 12:30",
+    "Mer 3": "Mercredi 13:30 - 15:30",
+    "Mer 4": "Mercredi 15:30 - 17:30",
+
+    "Jeu 1": "Jeudi 8:30 - 10:30",
+    "Jeu 2": "Jeudi 10:30 - 12:30",
+    "Jeu 3": "Jeudi 13:30 - 15:30",
+    "Jeu 4": "Jeudi 15:30 - 17:30",
+
+    "Ven 1": "Vendredi 8:30 - 10:30",
+    "Ven 2": "Vendredi 10:30 - 12:30",
+    "Ven 3": "Vendredi 13:30 - 15:30",
+    "Ven 4": "Vendredi 15:30 - 17:30",
+
+    "Sam 1": "Samedi 8:30 - 10:30",
+    "Sam 2": "Samedi 10:30 - 12:30"
+}
+let matieres = {
+    "prof1": {
+        "liste_matiere": ["matiere1", "matiere2"],
+        "disp": ["Lun1", "Lun2", "Mardi"]
+    },
+    "prof2": {
+        "liste_matiere": ["matiere1", "matiere2"],
+        "disp": ["Lun1", "Lun2", "Mardi"]
+    },
+    "prof3": {
+        "liste_matiere": ["matiere1", "matiere2"],
+        "disp": ["Lun1", "Lun2", "Mardi"]
+    },
+    "prof4": {
+        "liste_matiere": ["matiere1", "matiere2"],
+        "disp": ["Lun1", "Lun2", "Mardi"]
+    }
+}
+
+let disp_profs = {
+    "prof1": ["Lundi", "Mardi"],
+    "prof2": ["Mercredi", "Jeudi"],
+    "prof3": ["Lundi", "Jeudi"],
+    "prof4": ["Vendredi", "Samedi"],
+    "prof5": ["Mardi", "Samedi"]
+}
+
+
---
+++ seances.js
@@ -0,0 +1,19 @@
+let seances = [
+    {
+        "classes": "info L1",
+        "matiere": "compta",
+        "prof": "prof1",
+        "horraire": ["8:30 - 10:30", "10:30 - 12:30"],
+        "jours": "Lundi",
+        "heure_restante": 10
+    },
+    {
+        "classes": "info L2",
+        "matiere": "compta",
+        "prof": "prof1",
+        "horraire": ["8:30 - 10:30", "10:30 - 12:30"],
+        "jours": "Lundi",
+        "heure_restante": 10
+    }
+]
+
