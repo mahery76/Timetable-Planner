81f1034e22a4eb993a5e27af5ad5ee633b3c46ad
mahery76 <randrianjafymahery@gmail.com>
2023-10-23 22:39:22 +0300

    merge complete

--- client/src/components/Admin/AdminTeacher/TeacherDetails.jsx
+++ client/src/components/Admin/AdminTeacher/TeacherDetails.jsx
@@ -3,10 +3,7 @@ import TeacherInfo from './TeacherDetails/TeacherInfo'
 import DispoTable from './TeacherDetails/DispoTable'
 function TeacherDetails() {
   return (
-<<<<<<< HEAD
       <div className=" mx-8 mt-4 bg-white p-4 rounded-xl flex  justify-evenly">
-=======
->>>>>>> 639a48ae036cf0f1ef1b887c8c4470aa1b68a033
         <TeacherInfo />
         <DispoTable/>
       </div>
