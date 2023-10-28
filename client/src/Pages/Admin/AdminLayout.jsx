0f0f6cb52c706188e21e0cb7517e008582ec0d22
mahery76 <randrianjafymahery@gmail.com>
2023-10-28 15:59:51 +0300

    logo done

--- client/src/Pages/Admin/AdminLayout.jsx
+++ client/src/Pages/Admin/AdminLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import { BellIcon, CalendarDaysIcon, CalendarIcon, PencilIcon, PencilSquareIcon, UserIcon } from '@heroicons/react/24/solid'
 import HeaderItem from '../../components/HeaderItem'
 import TodayDate from '../../components/TodayDate'
+import logoEjeryemploi from '../../Assets/logo.png'
 
 function AdminLayout() {
   return (
@@ -10,9 +11,10 @@ function AdminLayout() {
 
       <div className='header__nav flex shadow-md top-0 bg-white h-20'>
 
-        <div className='w-52 ml-14 flex flex-col items-center'>
-          <div className='font-bold text-2xl pt-4 text-sky-700' >Ejeryemploi</div>
-          <div className='text-xs pb-2 text-sky-700'><TodayDate /></div>
+        <div className='w-52 ml-14 flex flex-col items-center justify-between'>
+          {/* <div className='font-bold text-2xl pt-4 text-sky-700' >Ejeryemploi</div> */}
+          <img src={logoEjeryemploi} className='mt-2 w-36' alt="" />
+          <div className='text-xs pb-2 text-sky-600 mt-12 font-bold absolute '><TodayDate /></div>
         </div>
 
         <div className='flex items-center w-full justify-center'>
