2645444974e015f837318e613f5101c82f5f3229
Mahery Antonio <randrianjafymahery@gmail.com>
2024-01-09 17:52:16 +0300

    add pool string connection to render pg

--- config/dbpg.js
+++ config/dbpg.js
@@ -1,11 +1,15 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
+
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "lesdapery",
+//     host: "localhost",
+//     port: 5432,
+//     database: "ejeryemploidb"
+// });
 
 const pool = new Pool({
-    user: "postgres",
-    password: "lesdapery",
-    host: "localhost",
-    port: 5432,
-    database: "ejeryemploidb"
+    connectionString: "postgres://ejeryemploidb_user:mPopMVq6Vz84JJhchNoAfK7WRdaCiabt@dpg-cmee89v109ks73c4hteg-a.oregon-postgres.render.com/ejeryemploidb"
+})
 
-});
 module.exports = pool
