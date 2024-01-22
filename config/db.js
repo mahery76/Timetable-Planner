35d51d183ba50dd8bf60d5922e66501018ecee7d
mahery76 <randrianjafymahery@gmail.com>
2024-01-22 11:12:28 +0100

    copied .env file

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize')
-const sequelize = new Sequelize('postgres://ejeryemploidb_user:mPopMVq6Vz84JJhchNoAfK7WRdaCiabt@dpg-cmee89v109ks73c4hteg-a.oregon-postgres.render.com/ejeryemploidb?ssl=true')
-// const sequelize = new Sequelize('ejeryemploidb', 'postgres', 'lesdapery', {
-//     host: 'localhost',
-//     dialect: 'postgres',
-//     logging: false,
-//     define: {
-//         freezeTableName: true 
-//     } 
-// })
+// const sequelize = new Sequelize('postgres://ejeryemploidb_user:mPopMVq6Vz84JJhchNoAfK7WRdaCiabt@dpg-cmee89v109ks73c4hteg-a.oregon-postgres.render.com/ejeryemploidb?ssl=true')
+const sequelize = new Sequelize('ejeryemploidb', 'postgres', 'lesdapery', {
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false,
+    define: {
+        freezeTableName: true 
+    } 
+})
 
 const testDbConnection = async () => {
     try {
